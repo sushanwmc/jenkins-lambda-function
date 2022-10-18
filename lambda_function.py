403802eff9062e98c400e3ea57cd67bc524f812a
sushanwmc <93652709+sushanwmc@users.noreply.github.com>
2022-10-18 15:23:31 +0545

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello World!')
+        'body': json.dumps('Hello World! This is the test from web hook trigger')
     }
