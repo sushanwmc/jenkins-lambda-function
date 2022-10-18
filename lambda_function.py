c4b83cabbe208173dcc5429cf1f11243d623d7a0
sushanwmc <93652709+sushanwmc@users.noreply.github.com>
2022-10-18 19:40:20 +0545

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello World! 2022')
+        'body': json.dumps('Hello World!coming soon 2023')
     }
