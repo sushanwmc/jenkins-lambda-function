d1ba2db7181c77c42fa6baec20827cf1ce36f12e
Sushan <sushan.stha.1995@gmail.com>
2022-11-10 17:36:42 +0545

    test00

--- lambda_function.py
+++ lambda_function.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello World!coming soon 2023, approval test, nov9, hellow world')
+        'body': json.dumps('Hello World!coming soon 2023, approval test, nov9, test')
     }
