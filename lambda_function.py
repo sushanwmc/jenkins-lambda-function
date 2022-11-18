a1b0b420eb1fa29f463ce661fc31a9016d4f5fc8
sushanwmc <93652709+sushanwmc@users.noreply.github.com>
2022-11-18 17:25:00 +0545

    Delete lambda_function.py

--- lambda_function.py
+++
@@ -1,8 +0,0 @@
-import json
-
-def lambda_handler(event, context):
-    # TODO implement
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello World!coming soon 2023, approval test, nov9, test112')
-    }
